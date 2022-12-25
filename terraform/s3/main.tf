e980dfa09d26e2f028673364c9a838324b7a2ef9
maxmlv <max.mlv.k@gmail.com>
2022-12-25 16:58:02 +0200

    finished with terraform

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -5,10 +5,38 @@ module "s3_bucket" {
 
   bucket = lower("${var.project_name}-images-${random_uuid.random.result}")
   acl = "public-read"
-  putin_khuylo = true
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  force_destroy = true
+
+  versioning = {
+    enabled = false
+  }
 
   tags = {
     Terraform = "true"
     Description = "${var.project_name} images S3 bucket"
   }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_bucket.s3_bucket_id
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Sid": "PublicRead",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+            "s3:GetObject"
+        ],
+        "Resource": "${module.s3_bucket.s3_bucket_arn}/*"
+    }
+]
+}
+EOF
 }
