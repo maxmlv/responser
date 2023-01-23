be3bd25a4f3a8c33303a889ed3a89fc80d157f5c
maxmlv <max.mlv.k@gmail.com>
2023-01-24 00:37:00 +0200

    vars file from argument

--- terraform/tf-output.sh
+++ terraform/tf-output.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ansible_vars=$1
+
 declare -a outputs=("rds_engine", "rds_address", "rds_port", "rds_db", "rds_username", "rds_password", "s3_bucket")
 
 for i in ${#outputs[@]}; do
     if [ $i == 0 ]; then
-        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" > ../ansible/vars/vars.yaml
+        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" > $ansible_vars
     else
-        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" >> ../ansible/vars/vars.yaml
+        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" >> $ansible_vars
     fi
 done
