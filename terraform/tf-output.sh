846397eadc4b0c98aba3bf3fc8db25d668378ddf
maxmlv <max.mlv.k@gmail.com>
2023-01-24 12:20:45 +0200

    fix: tf-outputs.sh

--- terraform/tf-output.sh
+++ terraform/tf-output.sh
@@ -2,14 +2,8 @@
 
 ansible_vars=$1
 
-declare -a outputs=("rds_engine", "rds_address", "rds_port", "rds_db", "rds_username", "rds_password", "s3_bucket")
+declare -a outputs=("rds_engine" "rds_address" "rds_port" "rds_db" "rds_username" "rds_password" "s3_bucket")
 
-for i in ${#outputs[@]}; do
-    if [ $i == 0 ]; then
-        echo ${outputs[$i]}
-        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" > $ansible_vars
-    else
-        echo ${outputs[$i]}
-        echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" >> $ansible_vars
-    fi
+for i in "${outputs[@]}"; do
+        echo "$i: $(terraform output -raw $i)" >> $ansible_vars
 done
