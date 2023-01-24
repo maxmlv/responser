85f294e0e9f97d7d74e96436063a41f3839bc1a4
maxmlv <max.mlv.k@gmail.com>
2023-01-24 12:00:25 +0200

    test tf-output.sh

--- terraform/tf-output.sh
+++ terraform/tf-output.sh
@@ -6,8 +6,10 @@ declare -a outputs=("rds_engine", "rds_address", "rds_port", "rds_db", "rds_user
 
 for i in ${#outputs[@]}; do
     if [ $i == 0 ]; then
+        echo ${outputs[$i]}
         echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" > $ansible_vars
     else
+        echo ${outputs[$i]}
         echo "${outputs[$i]}: $(terraform output -raw ${outputs[$i]})" >> $ansible_vars
     fi
 done
